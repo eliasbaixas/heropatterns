ba94ca23f9dd41abaeea0e32f0ed1e868044815a
elias <elias.baixas@gmail.com>
2018-04-13 07:20:23 +0200

    Fixes

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat svgs|while read i
+cat svgs | while read i
 do
-  curl $i > svg/$(basename $i) && unzip -d svg svg/$(basename $i)
+  curl $i > svg/$(basename $i) && unzip -o -d svg svg/$(basename $i)
 done
